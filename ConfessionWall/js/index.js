fc96e16e2ea2a75e2c99b08917d2c510ed222da6
Rosyr <syr050301@qq.com>
2023-09-26 09:39:43 +0800

    Fourth Commit

---
+++ ConfessionWall/js/index.js
@@ -0,0 +1,16 @@
+$(function(){
+	$("#top a").mouseover(function(){
+		$(this).stop(true, false).animate({
+			"background-color":"#c14000",
+			"border-color":"#f37126",
+			"color":"#fcd2d1"
+			}, 200);
+	})
+	$("#top a").mouseout(function(){
+		$(this).stop(true, false).animate({
+			"background-color":"#e5a65e",
+			"border-color":"#c14000",
+			"color":"#000"
+			}, 200);
+	})
+})
